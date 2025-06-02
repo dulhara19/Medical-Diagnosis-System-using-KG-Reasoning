ad3f06ef7eb22c411afa1a18130631ec0e7e5a25
dulhara <wkldulhara@gmail.com>
2025-06-02 23:40:32 +0530

    started developin new agent

---
+++ test.py
@@ -0,0 +1,3 @@
+from neragent import extract_symptoms_ner
+
+nswer=extract_symptoms_ner("Patient has a headache and fever, but no other symptoms.but he was having muscle pain yesteray. dizzy and vomiting as well. He was also having a runny nose and sore throat last week. He has been feeling fatigued for the past few days, but no other symptoms are present.also my head was hurting a lot")
